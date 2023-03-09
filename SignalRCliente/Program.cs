d9c685d1c1a190406af95597bbfe81ea694edd32
Paulo Diego <paulo.diego@unip.br>
2023-03-09 19:37:12 -0300

    Teste inicial

---
+++ SignalRCliente/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+const string url = "https://localhost:44339/messageHub";
+
+await using HubConnection? connection = new HubConnectionBuilder()
+    .WithUrl(url)
+    .Build();
+
+await connection.StartAsync();
+
+await foreach (DateTime date in connection.StreamAsync<DateTime>("Streaming"))
+{
+    Console.WriteLine(date);
+}
