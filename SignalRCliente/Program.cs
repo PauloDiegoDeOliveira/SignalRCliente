42edfeef47177f108b6692644544d073fef9a062
Paulo Diego <paulo.diego@unip.br>
2023-03-10 15:00:20 -0300

    Url

--- SignalRCliente/Program.cs
+++ SignalRCliente/Program.cs
@@ -4,7 +4,7 @@ const string url = "https://localhost:44339/messageHub";
 
 await using var connection = new HubConnectionBuilder()
     .WithUrl(url, options => options.HttpMessageHandlerFactory = _ => new HttpClientHandler { ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator })
-    //.WithUrl(url)
+    // .WithUrl(url)
     .Build();
 
 await connection.StartAsync();
